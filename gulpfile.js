f96452d3964ea370b83d170e7de4e028979155ec
mgrsskls <mail@mgrossklaus.de>
2019-02-09 13:14:03 +0100

    inline css

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,31 @@ const eslint = require("gulp-eslint");
 const gulp = require("gulp");
 const rollup = require("rollup");
 const terser = require("rollup-plugin-terser");
-// const postcss = require("gulp-postcss");
+const postcss = require("gulp-postcss");
 const stylelint = require("gulp-stylelint");
+const inlineSource = require("gulp-inline-source");
+const replace = require("gulp-replace");
 
+const sourceFolder = "source/";
 const buildFolder = "docs/";
-const jsFiles = "source/js/index.js";
-const cssFiles = "source/css/index.css";
+const jsFiles = `${sourceFolder}js/index.js`;
+const cssFiles = `${sourceFolder}css/index.css`;
 const jsDist = `${buildFolder}js/`;
 const cssDist = `${buildFolder}css/`;
+const htmlFile = "index.html";
+
+gulp.task("inline:css", () => {
+  return gulp
+    .src(htmlFile)
+    .pipe(gulp.dest(buildFolder))
+    .pipe(replace(sourceFolder, "/"))
+    .pipe(
+      inlineSource({
+        compress: false
+      })
+    )
+    .pipe(gulp.dest(buildFolder));
+});
 
 gulp.task("lint:js", () =>
   gulp
@@ -56,16 +73,25 @@ gulp.task("build:js", () => {
 gulp.task("build:css", () =>
   gulp
     .src(cssFiles, { sourcemaps: true })
-    // .pipe(postcss())
-    .pipe(gulp.dest(cssDist, { sourcemaps: "." }))
+    .pipe(postcss())
+    .pipe(gulp.dest(cssDist, { sourcemaps: "../" }))
 );
 
 gulp.task("clean", () => {
   return del([`${buildFolder}**/*`]);
 });
 
+gulp.task("delete:css", () => {
+  return del(cssDist);
+});
+
 gulp.task(
   "build",
-  gulp.series("clean", gulp.parallel("build:js", "build:css"))
+  gulp.series(
+    "clean",
+    gulp.parallel("build:js", "build:css"),
+    "inline:css",
+    "delete:css"
+  )
 );
 gulp.task("default", gulp.parallel("watch:lint:js", "watch:lint:css"));
